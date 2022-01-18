a80e130745761db1617219a087a688a024723235
daa-wtag <abdullah.daihan@welldev.io>
2022-01-18 11:35:10 +0600

    completed

---
+++ TableviewCell from xib/NameTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  basicTableViewCell.swift
+//  TableviewCell from xib
+//
+//  Created by Abdullah Mohammad Daihan on 18/1/22.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
--- TableviewCell from xib/ViewController.swift
+++ TableviewCell from xib/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let name = ["Daihan", "Linkon", "Fahim"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "NameTableViewCell", bundle: nil), forCellReuseIdentifier: "NameCell")
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath) as! NameTableViewCell
+        cell.nameLabel.text = name[indexPath.row]
+        return cell
+    }
 }
 
