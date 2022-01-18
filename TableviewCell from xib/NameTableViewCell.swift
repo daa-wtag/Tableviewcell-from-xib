3fc0f012f6fc34ca29c1d280da442609d40920d7
daa-wtag <abdullah.daihan@welldev.io>
2022-01-18 11:50:25 +0600

    none added

--- TableviewCell from xib/NameTableViewCell.swift
+++ TableviewCell from xib/NameTableViewCell.swift
@@ -10,4 +10,9 @@ import UIKit
 class NameTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+    }
 }
